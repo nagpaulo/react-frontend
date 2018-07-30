ee01a85899a98a9f263fe10229742051058d5df6
nagpaulo <nagpaulo@gmail.com>
2018-07-29 22:58:49 -0300

    Finalizando Todo

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,23 +1,17 @@
-import React, { Component } from 'react';
-import axios from 'axios'; 
+import React from 'react';
 
 import PageHeader from '../template/pageHeader';
 import Pagination from '../template/pagination';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
-export default class Todo extends Component{
-
-    render(){
-        return (
-            <div>
-                <PageHeader name="Tarefas" small="Cadastro"/>
-                <TodoForm /> 
-                <TodoList />
-                {/* <Pagination 
-                    result={this.state.result} 
-                    handlePaginationChange={this.handlePaginationChange}/> */}
-            </div>
-        )
-    }
-}
+export default props => (
+    <div>
+        <PageHeader name="Tarefas" small="Cadastro"/>
+        <TodoForm /> 
+        <TodoList />
+        {/* <Pagination 
+            result={this.state.result} 
+            handlePaginationChange={this.handlePaginationChange}/> */}
+    </div>
+);
